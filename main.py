66ebfff0a5d4ae6f5c3dd69eea1211d4ec966574
tylangzi <116060425+tylangzi@users.noreply.github.com>
2022-10-20 21:57:21 +0800

    Add files via upload
    最新3

--- main.py
+++ main.py
@@ -393,168 +393,173 @@ def main(z_name):
                 pt.hotkey('f11')
                 # time.sleep(10)
                 for li in lis_head:
-                    action = ActionChains(driver)
-                    action.scroll_to_element(li).perform()
-                    slack_link = pc.paste()
-
-                    # 获取commit
-                    commit_re_compile = re.compile(r"Commit:.{8}")
-                    commit_info = commit_re_compile.findall(li.text)[0].replace("Commit:", "").strip()
-                    # print("commit:",commit)
-
-                    re_compile = re.compile(r'Date:.{10}')
-                    print(re_compile.findall(li.text)[0].replace("Date:2022/", "").strip())
-                    ceshi_date = re_compile.findall(li.text)[0].replace("Date:2022/", "").strip()
-                    # slack_re_compile = re.compile(r"link:.*")
-                    # slack_link = slack_re_compile.findall(li.text)[0].replace("link:", "").strip()
-                    body_element = li.find_element(By.XPATH, "./../../..")
-                    lis_holetext = li.find_elements(By.XPATH, "./../../../p")
-                    index = 1
-                    for p in lis_holetext:
+                    if lis_head.index(li) == 0:
                         action = ActionChains(driver)
-                        action.scroll_to_element(p).perform()
-                        # time.sleep(0.5)
-                        if "@" not in p.text:
-                            # print(p.text)
-                            # 获取问题描述信息
-                            try:
-                                body_element_re_compile = re.compile(r"{}\s+\(.*\)".format(p.text))
-                                anser_descrip = body_element_re_compile.findall(body_element.text)[0].strip()
-                            except Exception as e:
-                                print(e)
-                                anser_descrip = p.text
-                            # print("问题描述",anser_descrip)
+                        action.scroll_to_element(li).perform()
+                        slack_link = pc.paste()
+    
+                        # 获取commit
+                        commit_re_compile = re.compile(r"Commit:.{8}")
+                        commit_info = commit_re_compile.findall(li.text)[0].replace("Commit:", "").strip()
+                        # print("commit:",commit)
 
-                            # 如果匹配到了多个anser_descrip,就不取括号
-                            try:
-                                is_none_anser_descrip = body_element_re_compile.findall(body_element.text)[1].strip()
-                                anser_descrip = p.text
-                            except Exception as e:
-                                print(e)
-                                # print("p.text", anser_descrip)
-
-                            # 获取问题xray链接
-                            xray_link = p.get_attribute('href')
-                            # print("xray链接：",xray_link)
-
-                            xpath = "//*[starts-with(text(),'Date')]/../../../../following-sibling::tr[{}]/td[2]".format(
-                                index)
-                            paste_loc = driver.find_element(By.XPATH, xpath)
+                        re_compile = re.compile(r'Date:.{10}')
+                        print(re_compile.findall(li.text)[0].replace("Date:2022/", "").strip())
+                        ceshi_date = re_compile.findall(li.text)[0].replace("Date:2022/", "").strip()
+                        # slack_re_compile = re.compile(r"link:.*")
+                        # slack_link = slack_re_compile.findall(li.text)[0].replace("link:", "").strip()
+                        body_element = li.find_element(By.XPATH, "./../../..")
+                        lis_holetext = li.find_elements(By.XPATH, "./../../../p")
+                        index = 1
+                        for p in lis_holetext:
                             action = ActionChains(driver)
-                            action.scroll_to_element(paste_loc).perform()
-                            html = """<p><a href="https://szexa.xray.autox.tech/?id=playback/pacifica-cn-371/20221019/2022-10-19-10-11-23&amp;t=3582.1#obstacleId=259093">Planning/Action/Static Nudge/No Trigger</a> (没有避让右测单车)</p>"""
-                            html = p.get_attribute("outerHTML")
-                            re_compile = re.compile(r'\W\w{5}\s+\d{1}\W')
-                            try:
-                                level_info = re_compile.findall(html)[0].strip()
-                                after_html = html.replace(level_info, "")
-                            except Exception as e:
-                                after_html = html
-                            # print("html",html)
-                            # html2 = """<p><a href="https://szexa.xray.autox.tech/?id=playback/pacifica-cn-371/20221019/2022-10-19-10-11-23&amp;t=3582.1#obstacleId=259093">Planning/Action/Static Nudge/No Trigger</a> </p>"""
-                            js = "arguments[0].innerHTML='" + after_html + "'"
-                            driver.execute_script(js, paste_loc)
+                            action.scroll_to_element(p).perform()
+                            # time.sleep(0.5)
+                            if "@" not in p.text:
+                                # print(p.text)
+                                # 获取问题描述信息
+                                try:
+                                    body_element_re_compile = re.compile(r"{}\s+\(.*\)".format(p.text))
+                                    anser_descrip = body_element_re_compile.findall(body_element.text)[0].strip()
+                                except Exception as e:
+                                    print(e)
+                                    anser_descrip = p.text
+                                # print("问题描述",anser_descrip)
 
-                            xpath = "//*[starts-with(text(),'Date')]/../../../../following-sibling::tr[{}]/td[1]".format(
-                                index)
-                            huibaoren_element = driver.find_element(By.XPATH, xpath)
+                                # 如果匹配到了多个anser_descrip,就不取括号
+                                try:
+                                    is_none_anser_descrip = body_element_re_compile.findall(body_element.text)[1].strip()
+                                    anser_descrip = p.text
+                                except Exception as e:
+                                    print(e)
+                                    # print("p.text", anser_descrip)
 
-                            genjin_people = "@xiaolonghuang {0} {1}".format(ceshi_date,commit_info)
+                                # 获取问题xray链接
+                                xray_link = p.get_attribute('href')
+                                # print("xray链接：",xray_link)
 
-                            js = "arguments[0].innerHTML='" + genjin_people + "'"
-                            driver.execute_script(js, huibaoren_element)
+                                xpath = "//*[starts-with(text(),'Date')]/../../../../following-sibling::tr[{}]/td[2]".format(
+                                    index)
+                                paste_loc = driver.find_element(By.XPATH, xpath)
+                                # action = ActionChains(driver)
+                                # action.scroll_to_element(paste_loc).perform()
+                                js = 'arguments[0].scrollIntoView()'
+                                driver.execute_script(js, paste_loc)
+                                html = """<p><a href="https://szexa.xray.autox.tech/?id=playback/pacifica-cn-371/20221019/2022-10-19-10-11-23&amp;t=3582.1#obstacleId=259093">Planning/Action/Static Nudge/No Trigger</a> (没有避让右测单车)</p>"""
+                                html = p.get_attribute("outerHTML")
+                                re_compile = re.compile(r'\W\w{5}\s+\d{1}\W')
+                                try:
+                                    level_info = re_compile.findall(html)[0].strip()
+                                    after_html = html.replace(level_info, "")
+                                except Exception as e:
+                                    after_html = html
+                                # print("html",html)
+                                # html2 = """<p><a href="https://szexa.xray.autox.tech/?id=playback/pacifica-cn-371/20221019/2022-10-19-10-11-23&amp;t=3582.1#obstacleId=259093">Planning/Action/Static Nudge/No Trigger</a> </p>"""
+                                js = "arguments[0].innerHTML='" + after_html + "'"
+                                driver.execute_script(js, paste_loc)
 
-                            index += 1
+                                xpath = "//*[starts-with(text(),'Date')]/../../../../following-sibling::tr[{}]/td[1]".format(
+                                    index)
+                                huibaoren_element = driver.find_element(By.XPATH, xpath)
 
-                        else:
-                            pass
-                    for i in range(1, index):
-                        print(index - i, "index")
-                        xpath = "//*[starts-with(text(),'Date')]/../../../../following-sibling::tr[{}]/td[2]".format(
-                            index - i)
-                        paste_loc = driver.find_element(By.XPATH, xpath)
+                                genjin_people = "@pinyihu"
 
-                        # 点击链接
-                        paste_loc_click = paste_loc.find_element(By.XPATH, "./p/a")
-                        paste_loc_click.click()
-                        # 截图
-                        element = driver.find_element(By.XPATH, "//*/span[@aria-label='Open link in a new tab']")
-                        js = "arguments[0].click()"
-                        driver.execute_script(js, element)
-                        print(driver.title)
-                        # 点击链接后重新获取handlelist
-                        handles_list = driver.window_handles
-                        if "xRay" not in driver.title:
-                            for window in handles_list:
-                                driver.switch_to.window(window)
-                                if "xRay" in driver.title:
-                                    # pen = driver.find_element(By.XPATH,
-                                    #                           "/html/body/div[1]/div/div[1]/div[2]/div[1]/div[1]/div/div/div/div[1]/div/div/div[2]/div[2]/div[3]/div[4]/div[6]")
-                                    time_element = WebDriverWait(driver, 100).until(
-                                        lambda x: x.find_element(By.XPATH, "//*/div[2]/div[4]/p[1]"))
-                                    break
-                        print(driver.title)
-                        # time.sleep(0.4)
-                        pt.hotkey('r')
-                        time.sleep(0.3)
-                        pt.hotkey('v')
-                        time.sleep(0.3)
-                        pt.hotkey('v')
-                        time.sleep(0.3)
-                        pt.hotkey('v')
-                        time.sleep(0.3)
-                        pt.hotkey('ctrl', 'r')
-                        time.sleep(0.3)
-                        pt.hotkey('enter')
-                        # 关闭窗口
-                        driver.close()
-                        # time.sleep(1)
-                        # 关闭后切换回来confluence_window
-                        print(confluence_window, "confluence_window")
-                        driver.switch_to.window(confluence_window)
-                        # break
+                                js = "arguments[0].innerHTML='" + genjin_people + "'"
+                                driver.execute_script(js, huibaoren_element)
 
-                        # 复制粘贴截图
-                        # time.sleep(0.8)
+                                index += 1
 
-                        # pt.hotkey('tab')
-                        # time.sleep(0.5)
-                        xpath = "//*[starts-with(text(),'Date')]/../../../../following-sibling::tr[{}]/td[3]".format(
-                            index - i)
-                        tupian_element = driver.find_element(By.XPATH, xpath)
-                        action = ActionChains(driver)
-                        action.scroll_to_element(tupian_element).perform()
-                        action.click(tupian_element).perform()
+                            else:
+                                pass
+                        for i in range(1, index):
+                            print(index - i, "index")
+                            xpath = "//*[starts-with(text(),'Date')]/../../../../following-sibling::tr[{}]/td[2]".format(
+                                index - i)
+                            paste_loc = driver.find_element(By.XPATH, xpath)
 
-                        pt.hotkey('ctrl', 'v')
-                        time.sleep(0.5)
-                        # pt.hotkey('right')
-                        xpath = "//*[starts-with(text(),'Date')]/../../../../following-sibling::tr[{}]/td[4]".format(
-                            index - i)
-                        tupian_element = driver.find_element(By.XPATH, xpath)
-                        action = ActionChains(driver)
-                        action.scroll_to_element(tupian_element).perform()
-                        action.click(tupian_element).perform()
-                        pc.copy(slack_link)
-                        pt.hotkey('ctrl', 'v')
-                        # js = "arguments[0].innerHTML='" + slack_link + "'"
-                        # driver.execute_script(js, tupian_element)
+                            # 点击链接
+                            paste_loc_click = paste_loc.find_element(By.XPATH, "./p/a")
+                            paste_loc_click.click()
+                            # 截图
+                            element = driver.find_element(By.XPATH, "//*/span[@aria-label='在新选项卡打开链接']")
+                            js = "arguments[0].click()"
+                            driver.execute_script(js, element)
+                            print(driver.title)
+                            # 点击链接后重新获取handlelist
+                            handles_list = driver.window_handles
+                            if "xRay" not in driver.title:
+                                for window in handles_list:
+                                    driver.switch_to.window(window)
+                                    if "xRay" in driver.title:
+                                        # pen = driver.find_element(By.XPATH,
+                                        #                           "/html/body/div[1]/div/div[1]/div[2]/div[1]/div[1]/div/div/div/div[1]/div/div/div[2]/div[2]/div[3]/div[4]/div[6]")
+                                        time_element = WebDriverWait(driver, 100).until(
+                                            lambda x: x.find_element(By.XPATH, "//*/div[2]/div[4]/p[1]"))
+                                        break
+                            print(driver.title)
+                            # time.sleep(0.4)
+                            pt.hotkey('r')
+                            time.sleep(0.3)
+                            pt.hotkey('v')
+                            time.sleep(0.3)
+                            pt.hotkey('v')
+                            time.sleep(0.3)
+                            pt.hotkey('v')
+                            time.sleep(0.3)
+                            pt.hotkey('ctrl', 'r')
+                            time.sleep(0.3)
+                            pt.hotkey('enter')
+                            # 关闭窗口
+                            driver.close()
+                            # time.sleep(1)
+                            # 关闭后切换回来confluence_window
+                            print(confluence_window, "confluence_window")
+                            driver.switch_to.window(confluence_window)
+                            # break
 
-                        i += 1
-                    # 清空表格
-                    source = driver.find_element(By.XPATH,
-                                                 "//*[starts-with(text(),'Date')]/../../../following-sibling::td[1]")
-                    target = driver.find_element(By.XPATH,
-                                                 "//*[starts-with(text(),'Date')]/../../../preceding-sibling::td[1]")
+                            # 复制粘贴截图
+                            # time.sleep(0.8)
 
-                    actions = ActionChains(driver)
-                    actions.drag_and_drop(source, target)
-                    actions.perform()
-                    pt.hotkey('backspace')
-                    pt.hotkey('f11')
-                    break
-                # except:
-                #     pass
+                            # pt.hotkey('tab')
+                            # time.sleep(0.5)
+                            xpath = "//*[starts-with(text(),'Date')]/../../../../following-sibling::tr[{}]/td[3]".format(
+                                index - i)
+                            tupian_element = driver.find_element(By.XPATH, xpath)
+                            action = ActionChains(driver)
+                            action.scroll_to_element(tupian_element).perform()
+                            action.click(tupian_element).perform()
+
+                            pt.hotkey('ctrl', 'v')
+                            time.sleep(0.5)
+                            # pt.hotkey('right')
+                            xpath = "//*[starts-with(text(),'Date')]/../../../../following-sibling::tr[{}]/td[4]".format(
+                                index - i)
+                            tupian_element = driver.find_element(By.XPATH, xpath)
+                            action = ActionChains(driver)
+                            action.scroll_to_element(tupian_element).perform()
+                            action.click(tupian_element).perform()
+                            pc.copy(slack_link)
+                            pt.hotkey('ctrl', 'v')
+                            # js = "arguments[0].innerHTML='" + slack_link + "'"
+                            # driver.execute_script(js, tupian_element)
+
+                            i += 1
+                        # 清空表格
+                        source = driver.find_element(By.XPATH,
+                                                     "//*[starts-with(text(),'Date')]/../../../following-sibling::td[1]")
+                        target = driver.find_element(By.XPATH,
+                                                     "//*[starts-with(text(),'Date')]/../../../preceding-sibling::td[1]")
+
+                        actions = ActionChains(driver)
+                        actions.drag_and_drop(source, target)
+                        actions.perform()
+                        pt.hotkey('backspace')
+                        pt.hotkey('f11')
+
+                    # except:
+                    #     pass
+                    else:
+                        pass
 
             if key == keyboard.Key.f2:
                 driver.implicitly_wait(100)
